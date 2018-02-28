d19284f9b9d407bb26370627b07ccbe1ac8fd718
Alan Christie <alan.christie@matildapeak.com>
2018-02-28 15:29:20 +0000

    - Adds missing Neo4j data loader entrypoint script

--- images/neo4j-data-loader/docker-entrypoint.sh
+++ images/neo4j-data-loader/docker-entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-# Copy the media directory to the mounted volume...
+# Copy the data directory to the mounted volume...
 echo "Copying media..."
-cp -R media/* /code/media
+cp -R data/* /data-loader
 # Touch (create) the loaded file to signal that the
 # data's present...
 echo "Done."
-touch /code/media/loaded
+touch /data-loader/loaded
