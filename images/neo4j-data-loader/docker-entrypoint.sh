ecdfc6a492a6386c6621c425bf3ae69bcba9fcd6
Alan Christie <alan.christie@matildapeak.com>
2018-03-01 08:30:00 +0000

    - Fixed neo4j-data-loader

--- images/neo4j-data-loader/docker-entrypoint.sh
+++ images/neo4j-data-loader/docker-entrypoint.sh
@@ -3,8 +3,10 @@
 set -e
 
 # Copy the data directory to the mounted volume...
+# And set typical execute permissions.
 echo "Copying media..."
 cp -R data/* /data-loader
+chmod +x /data-loader/*.sh
 # Touch (create) the loaded file to signal that the
 # data's present...
 echo "Done."
