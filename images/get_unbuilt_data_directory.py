2a581953e45f3d305da27ed5a6f4090c89038689
Alan Christie <alan.christie@matildapeak.com>
2018-05-10 15:20:52 +0100

    - Logging tweak

--- images/get_unbuilt_data_directory.py
+++ images/get_unbuilt_data_directory.py
@@ -161,16 +161,14 @@ else:
         if labels and DATA_ORIGIN_KEY in labels:
             image_data_origin = labels[DATA_ORIGIN_KEY]
         else:
-            LOGGER.warning('Image (%s) has no "%s" label',
-                           image_spec, DATA_ORIGIN_KEY)
+            LOGGER.warning('Image has no "%s" label', DATA_ORIGIN_KEY)
 
     # If the current label matches the most recent data directory
     # then there's nothing to do -
     # the latest image is build from the latest data directory.
     if image_str_info and image_data_origin == most_recent_data_dir:
-        LOGGER.info('The Latest image (%s) is already built from'
-                    ' the most recent data (%s)',
-                    image_spec, image_data_origin)
+        LOGGER.info('The latest image is already built from'
+                    ' the most recent data (%s)', image_data_origin)
         sys.exit(0)
     elif image_str_info:
         LOGGER.info('Latest image (%s) is built from %s, not %s',
@@ -182,5 +180,5 @@ else:
 # There is no image, or its label does not match the
 # most recent data directory and so we print the
 # data directory, which can be used to trigger a build...
-LOGGER.info('Data needs building (%s)', most_recent_data_dir)
+LOGGER.info('A new image needs building (%s)', most_recent_data_dir)
 print(most_recent_data_dir)
