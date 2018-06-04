aead4e8ac1aaac57e606800c6191b673083ef3d4
Alan Christie <alan.christie@matildapeak.com>
2018-06-04 14:19:00 +0100

    - Load script now creates 'loaded' file in data-loader directory   (could be used by a liveness probe script to detect the 'loading' phase) - Disabled liveness/readiness probes

--- images/graph/data-loader/load_neo4j.sh
+++ images/graph/data-loader/load_neo4j.sh
@@ -6,14 +6,15 @@ DATABASE=graph.db
 # then leave...
 if [ ! -d /data/databases/${DATABASE} ]; then
 
-    echo "Importing into '${DATABASE}'..."
+    echo "(load_neo4j.sh) Importing into '${DATABASE}'..."
     cd /data-loader
     /var/lib/neo4j/bin/neo4j-admin import \
         --database ${DATABASE} \
         --nodes "nodes-header.csv,nodes.csv" \
         --relationships:F2EDGE "edges-header.csv,edges.csv"
 
-    echo "Done."
+    echo "(load_neo4j.sh) Imported."
+    touch loaded
     cd /var/lib/neo4j
 
 fi
