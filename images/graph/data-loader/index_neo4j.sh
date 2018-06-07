54217a319b6452f5603957eb33f0106c1a481c4e
Anthony Bradley <anthony.richard.bradley@gmail.com>
2018-06-07 18:51:22 +0100

    Update index_neo4j.sh

--- images/graph/data-loader/index_neo4j.sh
+++ images/graph/data-loader/index_neo4j.sh
@@ -1,5 +1,5 @@
  #!/usr/bin/env bash
-/var/lib/neo4j/bin/neo4j-shell -p ${DATABASE} -f << EOF
+/var/lib/neo4j/bin/neo4j-shell -p /data/databases/graph.db -f << EOF
 CREATE INDEX ON :F2(smiles);
 schema await
 EOF
