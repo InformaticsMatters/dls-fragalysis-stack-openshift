c69d1bd0fe745120c9ed949390553aee16f43960
Alan Christie <alan.christie@matildapeak.com>
2018-09-27 12:26:54 +0100

    - First working (mailgun) solution

--- images/security-probe/probe.py
+++ images/security-probe/probe.py
@@ -11,39 +11,53 @@ a short period of time, until the service stops responding.
 The behaviour of the application is controlled by a number of
 'required' environment variables: -
 
-PROBE_DEPLOYMENT    The name of the deployment that should be scaled-down
-                    on probe failure.
+PROBE_LOCATION          The rest location to probe.
+                        This is typically a path and service
+                        (rather than a route) endpoint.
 
-PROBE_LOCATION      The rest location to probe.
-                    This is typically a path and service (rather than a route)
-                    endpoint.
+PROBE_RECIPIENTS        A comma-separated list of email recipients.
+                        This is optional, if not provided no email will be
+                        sent.
 
-PROBE_NAMESPACE     The name of the deployment's namespace.
+PROBE_OC_PASSWORD       The OpenShift user's password
 
-PROBE_RECIPIENTS    A comma-separated list of email recipients.
-                    This is optional, if not provided no email will be
-                    sent.
+PROBE_MAILGUBN_LOGIN    The Mailgun account login.
 
-PROBE_OC_HOST       A valid OpenShift host
-
-PROBE_OC_USER       A valid OpenShift host user
-
-PROBE_OC_PAsSWORD   The OpenShift user's password
+PROBE_MAILGUBN_PASSWORD The Mailgun account password.
 
 And a number of optional environment variables: -
 
-PROBE_PERIOD_M      The period between probes (minutes)
-                    (default 5)
+PROBE_DEPLOYMENT        The name of the deployment that should be scaled-down
+                        on probe failure.
+                        (default 'web')
 
-PROBE_THRESHOLD     The number of failures after which
-                    the service will be suspended.
-                    (default 2)
+PROBE_NAMESPACE         The name of the deployment's namespace.
+                        (default 'fragalysis-cicd')
+
+PROBE_NAMESPACE_H       The human name of the deployment's namespace.
+                        For use in email transmissions.
+                        (default 'Development')
+
+PROBE_OC_HOST           A valid OpenShift host
+                        (default 'openshift.xchem.diamond.ac.uk')
+
+PROBE_OC_USER           A valid OpenShift host user
+                        (default 'admin')
+
+PROBE_PERIOD_M          The period between probes (minutes)
+                        (default 5)
+
+PROBE_THRESHOLD         The number of failures after which
+                        the service will be suspended.
+                        (default 2)
 
 This Job may also terminate if there in an unrecoverable
 error condition whereby it will log the condition but exit with
 a code of 0 to avoid being re-spawned by OpenShift.
 """
 
+from email.mime.text import MIMEText
+from datetime import datetime
 import os
 import requests
 import smtplib
@@ -51,34 +65,45 @@ import sys
 import subprocess
 import time
 
-DEPLOYMENT_ENV = 'PROBE_DEPLOYMENT'
-DEPLOYMENT = os.environ.get(DEPLOYMENT_ENV)
 LOCATION_ENV = 'PROBE_LOCATION'
 LOCATION = os.environ.get(LOCATION_ENV)
-NAMESPACE_ENV = 'PROBE_NAMESPACE'
-NAMESPACE = os.environ.get(NAMESPACE_ENV)
 RECIPIENTS_ENV = 'PROBE_RECIPIENTS'
 RECIPIENTS = os.environ.get(RECIPIENTS_ENV)
 
+DEPLOYMENT_ENV = 'PROBE_DEPLOYMENT'
+DEPLOYMENT = os.environ.get(DEPLOYMENT_ENV, 'web')
+NAMESPACE_ENV = 'PROBE_NAMESPACE'
+NAMESPACE = os.environ.get(NAMESPACE_ENV, 'fragalysis-cicd')
+NAMESPACE_H_ENV = 'PROBE_NAMESPACE_HUMAN'
+NAMESPACE_H = os.environ.get(NAMESPACE_H_ENV, 'Development')
 PERIOD_M_ENV = 'PROBE_PERIOD_M'
 PERIOD_M = os.environ.get(PERIOD_M_ENV, '5')
 THRESHOLD_ENV = 'PROBE_THRESHOLD'
 THRESHOLD = os.environ.get(THRESHOLD_ENV, '2')
 
-OC_HOST = os.environ.get('PROBE_OC_HOST')
-OC_USER = os.environ.get('PROBE_OC_USER')
+OC_HOST = os.environ.get('PROBE_OC_HOST', 'openshift.xchem.diamond.ac.uk')
+OC_USER = os.environ.get('PROBE_OC_USER', 'admin')
 OC_PASSWORD = os.environ.get('PROBE_OC_PASSWORD')
+
+# SMTP (Mailgun) details...
+MAILGUN_LOGIN = os.environ.get('PROBE_MAILGUN_LOGIN')
+MAILGUN_PASSWORD = os.environ.get('PROBE_MAILGUN_PASSWORD')
+MAILGUN_ADDR = 'smtp.mailgun.org'
+MAILGUN_PORT = 587
+
+# The email address of the Security Probe
+PROBE_EMAIL = 'Security Probe <dls.security.probe@informaticsmatters.com>'
 
 # The period of time to pause, once we're initialised,
 # prior to entering the probe loop
-PRE_PROBE_DELAY_S = 2.0
+PRE_PROBE_DELAY_S = 10.0
 
 # Timeout of the probe call
-PROBE_TIMEOUT_S = 2.0
+PROBE_TIMEOUT_S = 4.0
 
 # The time (in seconds) to wait after suspending the
 # service to wait for a los of response.
-POST_TERMINATE_PERIOD_S = 60
+POST_TERMINATE_PERIOD_S = 120
 # The polling period for the probe after terminating.
 POST_TERMINATE_PROBE_PERIOD_S = 5
 
@@ -89,8 +114,17 @@ def error(msg):
     :param msg: A short error message
     :type msg: ``String``
     """
-    print('ERROR: %s' % msg)
+    message('ERROR: %s' % msg)
     sys.exit(0)
+
+
+def warning(msg):
+    """Prints the supplied warning message.
+
+    :param msg: A short message
+    :type msg: ``String``
+    """
+    message('WARNING: %s' % msg)
 
 
 def message(msg):
@@ -99,22 +133,99 @@ def message(msg):
     :param msg: The message
     :type msg: ``String``
     """
-    print('-) %s' % msg)
+    print('-) [%s] %s' % (datetime.now(), msg))
 
 
 def email_warning():
-    """Sends a warning email"""
-    print('-) Sent warning email')
+    """Sends an email, driven by the first probe failure.
+
+    This email delivers a warning that the first probe attempt has
+    failed. Another email will come when the probe fails a second time.
+    """
+    msg = MIMEText("The Fragalysis %s Project's Security Probe"
+                   " has detected an initial failure.\n\n"
+                   "The Security Probe will run again in %s minutes.\n\n"
+                   "After %s failures (this counts as one)"
+                   " the %s service will be suspended."
+                   % (NAMESPACE_H, PERIOD_M, THRESHOLD, NAMESPACE_H),
+                   _charset='utf-8')
+
+    msg['Subject'] = 'Fragalysis %s Project Security Probe Failure' \
+                     ' - First Event' % NAMESPACE_H
+    msg['From'] = PROBE_EMAIL
+    msg['To'] = RECIPIENTS
+
+    smtp = smtplib.SMTP(MAILGUN_ADDR, MAILGUN_PORT)
+    rv = smtp.login(MAILGUN_LOGIN, MAILGUN_PASSWORD)
+    if rv[0] == 235:
+        smtp.sendmail(PROBE_EMAIL,
+                      RECIPIENTS.split(','),
+                      msg.as_string())
+    smtp.quit()
+
+    message('Sent warning email')
 
 
 def email_suspension():
-    """Sends a service suspension email"""
-    print('-) Sent suspension email')
+    """Sends an email, driven by the final probe failure.
+
+    This email delivers a warning that the final security probe attempt has
+    failed and the probed service is being shutdown.
+    """
+    msg = MIMEText("The Fragalysis %s Project's Security Probe"
+                   " has detected too many failures.\n\n"
+                   "The service is now being suspended.\n\n"
+                   "You should check the service implementation"
+                   " and deploy a working solution."
+                   % NAMESPACE_H,
+                   _charset='utf-8')
+
+    msg['Subject'] = 'Fragalysis %s Project Security Probe Failure' \
+                     ' - Service Suspended' % NAMESPACE_H
+    msg['From'] = PROBE_EMAIL
+    msg['To'] = RECIPIENTS
+
+    smtp = smtplib.SMTP(MAILGUN_ADDR, MAILGUN_PORT)
+    rv = smtp.login(MAILGUN_LOGIN, MAILGUN_PASSWORD)
+    if rv[0] == 235:
+        smtp.sendmail(PROBE_EMAIL,
+                      RECIPIENTS.split(','),
+                      msg.as_string())
+    smtp.quit()
+
+    message('Sent suspension email')
 
 
 def email_suspension_failure():
-    """Sends a fail to suspend service email"""
-    print('-) Sent suspension failure email')
+    """Sends an email, driven by the failure to suspend the service.
+
+    This email delivers a warning that the suspension of the probed
+    service failed and there is a possibility that it is still running
+    and vulnerable.
+    """
+    msg = MIMEText("The Fragalysis %s Project's Service"
+                   " has failed to respond to a suspension request."
+                   " This is unexpected and leaves the service deployed"
+                   " and vulnerable.\n\n"
+                   "Please seek the urgent advice of a"
+                   " cluster administrator."
+                   % NAMESPACE_H,
+                   _charset='utf-8')
+
+    msg['Subject'] = 'Fragalysis %s Project Security Probe Failure' \
+                     ' - Service Suspension Failure' % NAMESPACE_H
+    msg['From'] = PROBE_EMAIL
+    msg['To'] = RECIPIENTS
+
+    smtp = smtplib.SMTP(MAILGUN_ADDR, MAILGUN_PORT)
+    rv = smtp.login(MAILGUN_LOGIN, MAILGUN_PASSWORD)
+    if rv[0] == 235:
+        smtp.sendmail(PROBE_EMAIL,
+                      RECIPIENTS.split(','),
+                      msg.as_string())
+    smtp.quit()
+
+    message('Sent suspension failure email')
 
 
 def probe():
@@ -141,14 +252,15 @@ def probe():
             count = resp.json()['count']
             if count:
                 ret_val = False
+                warning('Ooops - received "count" value of %d' % count)
         else:
             # Count not in the response. Odd?
-            message('WARNING: "count" not in the response')
+            warning('"count" not in the response')
     else:
         if resp:
-            message('WARNING: Got status %d' % resp.status_code)
+            warning('Got status %d' % resp.status_code)
         else:
-            message('WARNING: Got no response')
+            warning('Got no response from location')
 
     return ret_val
 
@@ -171,6 +283,13 @@ if not LOCATION:
 # A deployment (behind the service we're monitoring) must be provided.
 if not DEPLOYMENT:
     error('The %s environment variable is not defined' % DEPLOYMENT_ENV)
+# Must have mailgun credentials
+if not MAILGUN_LOGIN:
+    error('The mailgun login is not defined')
+if not MAILGUN_PASSWORD:
+    error('The mailgun password is not defined')
+
+# Conversions.
 # Period must be a number
 try:
     period_s_int = int(PERIOD_M) * 60
@@ -191,7 +310,7 @@ message('LOCATION="%s"' % LOCATION)
 message('PERIOD_M=%s' % PERIOD_M)
 message('THRESHOLD=%s' % THRESHOLD)
 
-message('Pre-probe delay (%s)...' % PRE_PROBE_DELAY_S)
+message('In pre-probe delay...')
 time.sleep(PRE_PROBE_DELAY_S)
 
 message('Probing...')
@@ -202,11 +321,8 @@ while not failed:
     # Probe (and deal with failure)
     if not probe():
         failure_count += 1
-        message('Ooops - count is %s.'
-                ' Probe failed (%d/%d).' % (count,
-                                            failure_count,
-                                            threshold_int))
-        if failure_count == 0:
+        message('Probe failed (%d/%d)' % (failure_count, threshold_int))
+        if failure_count == 1:
             email_warning()
         elif failure_count >= threshold_int:
             message('Reached failure threshold')
@@ -229,53 +345,66 @@ email_suspension()
 
 # Login
 #
+message('Logging in to OpenShift...')
 cmd = 'oc login %s -u %s -p %s' % (OC_HOST, OC_USER, OC_PASSWORD)
-result = subprocess.run(cmd.split())
+result = subprocess.run(cmd.split(),
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE)
 suspended = False
 if result.returncode:
     # Login failed!
     message('Login failed!')
-    pass
 else:
-    message('Logged in!')
+    message('Logged in')
 
     # Project
     #
+    message('Switching namespace...')
     cmd = 'oc project %s' % NAMESPACE
-    result = subprocess.run(cmd.split())
+    result = subprocess.run(cmd.split(),
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
     if result.returncode:
-        # Login failed!
+        # Project movement failed!
         message('Project failed!')
-        pass
     else:
-        message('Moved project!')
+        message('Switched')
 
         # Scale
         #
         #cmd = 'oc scale dc %s --replicas=0' % DEPLOYMENT
+        message('Suspending service...')
         cmd = 'oc get projects'
-        result = subprocess.run(cmd.split())
+        result = subprocess.run(cmd.split(),
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
         if result.returncode:
             # The scaling command failed!
-            pass
-        message('Suspended - waiting...')
+            message('Suspension failed!')
+        else:
+            message('Suspended')
 
-        # Continue to probe until success...
-        waited_s = 0
-        waited_long_enough = False
-        while not waited_long_enough:
-            if probe():
-                suspended = True
-                waited_long_enough = True
-            else:
-                time.sleep(POST_TERMINATE_PROBE_PERIOD_S)
-                waited_s += POST_TERMINATE_PROBE_PERIOD_S
-                if waited_s >= POST_TERMINATE_PERIOD_S:
+            # Continue to probe until success
+            #
+            message('Waiting for service termination...')
+            waited_s = 0
+            waited_long_enough = False
+            while not waited_long_enough:
+                if probe():
+                    suspended = True
                     waited_long_enough = True
+                    message('Terminated')
+                else:
+                    time.sleep(POST_TERMINATE_PROBE_PERIOD_S)
+                    waited_s += POST_TERMINATE_PROBE_PERIOD_S
+                    if waited_s >= POST_TERMINATE_PERIOD_S:
+                        waited_long_enough = True
 
 # If we failed to suspend the service...
 if not suspended:
+    warning('Service failed to respond to suspension!')
     email_suspension_failure()
 
 # We leave without an error.
 # The Job will terminate until we're run again.
+message("That's all Folks!")
