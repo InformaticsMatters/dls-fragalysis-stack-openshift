3cad872f8e3c4180e42b1d270d64f77f1cfc97ad
Alan Christie <alan.christie@matildapeak.com>
2018-11-05 14:25:27 +0000

    - Now gets and sets views - Simplified execution

--- jenkins/configure-cicd.py
+++ jenkins/configure-cicd.py
@@ -20,9 +20,8 @@ import yaml
 
 from im_jenkins_server import ImJenkinsServer
 
-# URLs for the test and production servers.
-SERVERS = {'test': 'jenkins-fragalysis-cicd.apps.xchem.diamond.ac.uk',
-           'prod': 'jenkins-fragalysis-cicd-prod.apps.xchem.diamond.ac.uk'}
+# URL for the CI/CD server.
+SERVER = 'jenkins-fragalysis-cicd.apps.xchem.diamond.ac.uk'
 # Our job configuration directory
 JOB_DIR = 'jobs'
 # CLUSTER_URL
@@ -60,18 +59,17 @@ PARSER.add_argument('action',
                     help='The action to perform. This is typically one'
                          ' of "get" in order to get the configuration or'
                          ' "set" to set the job configuration for a server.')
-PARSER.add_argument('server',
-                    choices=['test', 'prod'],
-                    help='The Jenkins server, typically one of "test" for'
-                         ' the test server or "prod" for the production'
-                         ' server.')
 PARSER.add_argument('-f','--force',
                     action='store_true',
                     help='Force the action (useful during set).')
+PARSER.add_argument('--disable-jobs',
+                    action='store_true',
+                    help='Disable the jobs as they are created or reconfigured'
+                         ' (useful during set).')
 ARGS = PARSER.parse_args()
 
 # Connect to the server
-J_URL = SERVERS[ARGS.server]
+J_URL = SERVER
 J_URL_WITH_USER = 'https://%s:%s@%s' % (J_USER, J_TOKEN, J_URL)
 JS = ImJenkinsServer(J_URL_WITH_USER)
 if not JS.is_connected():
@@ -83,8 +81,10 @@ if ARGS.action == 'get':
 
     LOGGER.info('Reading server configuration...')
     # You can't get the credentials!
-    # Read Jobs into 'jobs-test' or 'jobs-prod'...
-    JS.get_jobs('jobs-' + ARGS.server)
+    # Read Jobs into 'jobs'...
+    JS.get_jobs('jobs')
+    # And views into 'views'...
+    JS.get_views('views')
 
 elif ARGS.action == 'set':
 
@@ -143,7 +143,9 @@ elif ARGS.action == 'set':
     JS.set_secret_text('ocUserPassword',
                        J_OC_USER_PASSWORD,
                        "The OC user password (for Agent-based OC commands)")
-    # Write Jobs from 'jobs-test' or 'jobs-prod'...
-    JS.set_jobs('jobs-' + ARGS.server, force=ARGS.force)
+    # Write Jobs from 'jobs'...
+    JS.set_jobs('jobs', set_disabled=ARGS.disable_jobs, force=ARGS.force)
+    # Write Views from 'views'...
+    JS.set_views('views', force=ARGS.force)
 
 LOGGER.info('Done')
